0ada10b859743866db0e2f13ccedc3cd43f5fb29
Sergio <sjmg@alu.ua.es>
2017-06-30 17:58:01 +0200

    inicial

---
+++ src/main/java/es/ua/springboot/repository/PlanetRepository.java
@@ -0,0 +1,13 @@
+package es.ua.springboot.repository;
+
+import es.ua.springboot.model.Planet;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by sjmg on 30/06/17.
+ */
+@Repository
+public interface PlanetRepository extends JpaRepository<Planet,Long{
+    Planet findByName(String name);
+    Planet findByNameLike(String name);
+}
